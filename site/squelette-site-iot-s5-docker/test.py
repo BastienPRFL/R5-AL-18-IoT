b5577e939595f1051b1b33e778408b4d295587a0
BastienPRFL <bastienperruffel@gmail.com>
2024-10-01 09:54:53 +0200

    Add données geoloc

---
+++ site/squelette-site-iot-s5-docker/test.py
@@ -0,0 +1,25 @@
+import json
+
+dic = {}
+i = 1
+
+with open('points-dapport-volontaire-dechets-et-moyens-techniques.json', 'r') as json_file:
+    datas = json.load(json_file)
+    """ print(json.dumps(datas, indent=4, sort_keys=True)) """
+    for data in datas:
+        if data['type_flux'] == 'verre':
+        
+
+            dic[i] = {
+                'lon' : data['geo_point_2d']['lon'],
+                'lat' : data['geo_point_2d']['lat'],
+            }
+            
+            i += 1
+            
+print(dic)
+
+with open('geoloc_datas.json', 'w') as f:
+    json.dump(dic, f, indent=2)
+
+
