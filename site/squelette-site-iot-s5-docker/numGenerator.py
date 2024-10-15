1f7a73dd2b814764c7e6eeb053ec4dfd8840eefe
LouisPnt <louispenet2004@gmail.com>
2024-10-15 16:00:17 +0200

    add mqtt to numGenerator

--- site/squelette-site-iot-s5-docker/numGenerator.py
+++ site/squelette-site-iot-s5-docker/numGenerator.py
@@ -2,6 +2,12 @@ import os
 import random
 import json
 import time
+import paho.mqtt.client as mqtt
+
+
+broker = "localhost"
+port = 1884
+topic = "recupverres"
 
 # Fonction pour générer un nombre aléatoire supérieur à la base
 def random_greater_than_base(base):
@@ -13,6 +19,7 @@ def random_greater_than_base(base):
 def read_json_file(file_path):
     with open(file_path, 'r') as file:
         return json.load(file)
+    
 
 # Fonction pour écrire les données mises à jour dans un fichier JSON
 def write_json_file(file_path, data):
@@ -24,7 +31,7 @@ current_dir = os.path.dirname(__file__)
 file_path = os.path.join(current_dir, 'nginx_html/geoloc_datas.json')
 
 # Boucle pour mettre à jour le fichier toutes les 30 secondes
-while True:
+"""while True:
     # Charger les données du fichier JSON existant
     geoloc_data = read_json_file(file_path)
 
@@ -43,3 +50,39 @@ while True:
 
     # Attendre 30 secondes avant la prochaine mise à jour
     time.sleep(30)
+"""
+
+def envoi_donnees_recupverres(client, recupverres):
+    dico_donnees = {}
+    for rec in recupverres:
+        #datas = rec.get('fields', {})
+        lon = recupverres[rec].get('lon')
+        lat = recupverres[rec].get('lat')
+        stock = random_greater_than_base(1)
+        dico_donnees[rec] = {
+            "lon": lon,
+            "lat": lat,
+            "stockage": stock
+        }
+
+    envoi_data = json.dumps(dico_donnees)
+    client.publish(topic,envoi_data)
+    print("Mise à jour des données")
+    print(envoi_data)
+
+
+
+recupverres = read_json_file("nginx_html/geoloc_datas.json")
+print("debut")
+
+def sur_connexion(client, user_datas, flags, retour):
+    print("Connecté à MQTT, code de retour : " + str(retour))
+
+client = mqtt.Client()
+client.on_connect = sur_connexion
+client.connect(broker, port, 60)
+client.loop_start()
+
+while True:
+        envoi_donnees_recupverres(client, recupverres)
+        time.sleep(30)
